fc54cb5a89be2ccdddd82a61b8e1088cd5856ac1
ericlin09 <ericlin09@gmail.com>
2024-11-25 23:15:25 +0800

    Add support platform, web-desktop

--- dist/builder.js
+++ dist/builder.js
@@ -2,6 +2,65 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.configs = exports.unload = exports.load = void 0;
 const global_1 = require("./global");
+const webZipBundleConfig = {
+    hooks: './hooks',
+    options: {
+        enable: {
+            label: `i18n:${global_1.PACKAGE_NAME}.options.enable`,
+            description: `i18n:${global_1.PACKAGE_NAME}.options.enable`,
+            default: true,
+            render: {
+                ui: 'ui-checkbox',
+            }
+        },
+        downloadZipAtIndexHtml: {
+            label: `i18n:${global_1.PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
+            description: `i18n:${global_1.PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
+            default: false,
+            render: {
+                ui: 'ui-checkbox',
+            }
+        },
+        selectPackSize: {
+            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSize`,
+            description: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeDescription`,
+            default: 'option2',
+            render: {
+                ui: 'ui-select',
+                items: [
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option0`,
+                        value: 'option0',
+                    },
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option1`,
+                        value: 'option1',
+                    },
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option2`,
+                        value: 'option2',
+                    },
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option3`,
+                        value: 'option3',
+                    },
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option4`,
+                        value: 'option4',
+                    },
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option5`,
+                        value: 'option5',
+                    },
+                    {
+                        label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option6`,
+                        value: 'option6',
+                    },
+                ],
+            },
+        },
+    },
+};
 const load = function () {
     (0, global_1.logDebug)(`${global_1.PACKAGE_NAME} load`);
 };
@@ -11,63 +70,6 @@ const unload = function () {
 };
 exports.unload = unload;
 exports.configs = {
-    'web-mobile': {
-        hooks: './hooks',
-        options: {
-            enable: {
-                label: `i18n:${global_1.PACKAGE_NAME}.options.enable`,
-                description: `i18n:${global_1.PACKAGE_NAME}.options.enable`,
-                default: true,
-                render: {
-                    ui: 'ui-checkbox',
-                }
-            },
-            downloadZipAtIndexHtml: {
-                label: `i18n:${global_1.PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
-                description: `i18n:${global_1.PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
-                default: false,
-                render: {
-                    ui: 'ui-checkbox',
-                }
-            },
-            selectPackSize: {
-                label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSize`,
-                description: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeDescription`,
-                default: 'option2',
-                render: {
-                    ui: 'ui-select',
-                    items: [
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option0`,
-                            value: 'option0',
-                        },
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option1`,
-                            value: 'option1',
-                        },
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option2`,
-                            value: 'option2',
-                        },
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option3`,
-                            value: 'option3',
-                        },
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option4`,
-                            value: 'option4',
-                        },
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option5`,
-                            value: 'option5',
-                        },
-                        {
-                            label: `i18n:${global_1.PACKAGE_NAME}.options.selectPackSizeOptions.option6`,
-                            value: 'option6',
-                        },
-                    ],
-                },
-            },
-        },
-    },
+    'web-mobile': webZipBundleConfig,
+    'web-desktop': webZipBundleConfig,
 };
--- source/builder.ts
+++ source/builder.ts
@@ -1,71 +1,74 @@
 import { BuildPlugin } from '../@types';
 import { PACKAGE_NAME, logDebug } from './global';
 
-export const load: BuildPlugin.load = function() {
+const webZipBundleConfig = {
+    hooks: './hooks',
+    options: {
+        enable: {
+            label: `i18n:${PACKAGE_NAME}.options.enable`,
+            description: `i18n:${PACKAGE_NAME}.options.enable`,
+            default: true,
+            render: {
+                ui: 'ui-checkbox',
+            }
+        },
+        downloadZipAtIndexHtml: {
+            label: `i18n:${PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
+            description: `i18n:${PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
+            default: false,
+            render: {
+                ui: 'ui-checkbox',
+            }
+        },
+        selectPackSize: {
+            label: `i18n:${PACKAGE_NAME}.options.selectPackSize`,
+            description: `i18n:${PACKAGE_NAME}.options.selectPackSizeDescription`,
+            default: 'option2',
+            render: {
+                ui: 'ui-select',
+                items: [
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option0`,
+                        value: 'option0',
+                    },
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option1`,
+                        value: 'option1',
+                    },
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option2`,
+                        value: 'option2',
+                    },
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option3`,
+                        value: 'option3',
+                    },
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option4`,
+                        value: 'option4',
+                    },
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option5`,
+                        value: 'option5',
+                    },
+                    {
+                        label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option6`,
+                        value: 'option6',
+                    },
+                ],
+            },
+        },
+    },
+};
+
+export const load: BuildPlugin.load = function () {
     logDebug(`${PACKAGE_NAME} load`);
 };
-export const unload: BuildPlugin.load = function() {
+export const unload: BuildPlugin.load = function () {
     logDebug(`${PACKAGE_NAME} unload`);
 };
 
 export const configs: BuildPlugin.Configs = {
-    'web-mobile': {
-        hooks: './hooks',
-        options: {
-            enable: {
-                label: `i18n:${PACKAGE_NAME}.options.enable`,
-                description: `i18n:${PACKAGE_NAME}.options.enable`,
-                default: true,
-                render: {
-                    ui: 'ui-checkbox',
-                }
-            },
-            downloadZipAtIndexHtml: {
-                label: `i18n:${PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
-                description: `i18n:${PACKAGE_NAME}.options.downloadZipAtIndexHtml`,
-                default: false,
-                render: {
-                    ui: 'ui-checkbox',
-                }
-            },
-            selectPackSize: {
-                label: `i18n:${PACKAGE_NAME}.options.selectPackSize`,
-                description: `i18n:${PACKAGE_NAME}.options.selectPackSizeDescription`,
-                default: 'option2',
-                render: {
-                    ui: 'ui-select',
-                    items: [
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option0`,
-                            value: 'option0',
-                        },
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option1`,
-                            value: 'option1',
-                        },
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option2`,
-                            value: 'option2',
-                        },
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option3`,
-                            value: 'option3',
-                        },
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option4`,
-                            value: 'option4',
-                        },
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option5`,
-                            value: 'option5',
-                        },
-                        {
-                            label: `i18n:${PACKAGE_NAME}.options.selectPackSizeOptions.option6`,
-                            value: 'option6',
-                        },
-                    ],
-                },
-            },
-        },
-    },
+    'web-mobile': webZipBundleConfig,
+    'web-desktop': webZipBundleConfig,
 };
